b95c10e2383d30c8d245a334cbdbd68c39869a5e
Roman Ivasyk <ivasykdok@gmail.com>
2020-09-17 12:44:04 +0300

    edit eshop

--- js/eshop.js
+++ js/eshop.js
@@ -5,6 +5,39 @@ $('document').ready(function () {
 function loadCart() {
   // завантаження товарів на сторінку
   $.getJSON('cart.json', function (data) {
-    console.log(data);
+    // console.log(data);
+    let out = '';
+    for(let key in data) {
+      out += '<div class="discount_slider__item">';
+      //insert image
+      out += '<div class="discount_slider__item___img">';
+        out += '<img src="'+data[key].image+'" alt="'+data[key].name+'">';
+      out += '</div>';
+      //end image
+
+      //insert description
+      out += '<div class="discount_slider__item___name">';
+        out += '<span>' + data[key].description + '</span>';
+      out += '</div>';
+      //end description
+
+      //insert price
+      out += '<div class="discount_slider__item___price">';
+        out += '<span class="discount_slider__item___price_a">' + '$' + data[key].coast  + '</span>';
+        out += '<span class="discount_slider__item___price_b">' + '$' + data[key].coast_b  + '</span>';
+      out += '</div>';
+      //end price
+
+      //btn
+      out += '<button class="btn discount_slider__item___btn">';
+        out += '<img src="'+ data[key].btnImg+'" alt="Supermarket">';
+        out += '<span>Add Tod Cart</span>'
+      out += '</button>';
+      //end btn
+
+      out += '</div>';
+
+    }
+    $('.discount_slider').html(out);
   });
 }
